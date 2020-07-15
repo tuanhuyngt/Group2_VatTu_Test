3460699a54c4b6d32139aff7df499c371ccff0ee
tuanhuyngt <tuanhuyngt@gmail.com>
2020-07-15 22:19:58 +0700

    init

---
+++ Group2_VatTu_Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Group2_VatTu_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
